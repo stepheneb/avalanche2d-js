e41e1047fb59ab110384d14ce685a2c1421d9e8b
Seth Tisue <seth@tisue.net>
2011-10-28 13:21:58 -0400

    3x speedup thanks to improved logic

--- scala/Bureaucrats.scala
+++ scala/Bureaucrats.scala
@@ -31,30 +31,30 @@ class Bureaucrats {
   def rand = util.Random.nextInt(dim)
   var total = 2 * dim * dim
   def go() {
-    var active = List((rand, rand))
+    var active: Seq[(Int, Int)] = List((rand, rand))
     N(active.head._1)(active.head._2) += 1
     total += 1
     while(active.nonEmpty) {
       var nextActive = collection.mutable.ListBuffer[(Int, Int)]()
-      val overloaded = active.filter(coords => N(coords._1)(coords._2) > 3)
-      for((x, y) <- overloaded) {
-        N(x)(y) -= 4
-        total -= 4
-        def offLoadToNeighbor(dx: Int, dy: Int) = {
-          val x2 = x + dx
-          val y2 = y + dy
-          if(x2 >= 0 && x2 < dim && y2 >= 0 && y2 < dim) {
-            N(x2)(y2) += 1
-            total += 1
-            nextActive += ((x2, y2))
+      for((x, y) <- active)
+        if(N(x)(y) > 3) {
+          N(x)(y) -= 4
+          total -= 4
+          def offLoadToNeighbor(dx: Int, dy: Int) = {
+            val x2 = x + dx
+            val y2 = y + dy
+            if(x2 >= 0 && x2 < dim && y2 >= 0 && y2 < dim) {
+              N(x2)(y2) += 1
+              total += 1
+              nextActive += ((x2, y2))
+            }
           }
+          offLoadToNeighbor(1, 0)
+          offLoadToNeighbor(-1, 0)
+          offLoadToNeighbor(0, 1)
+          offLoadToNeighbor(0, -1)
         }
-        offLoadToNeighbor(1, 0)
-        offLoadToNeighbor(-1, 0)
-        offLoadToNeighbor(0, 1)
-        offLoadToNeighbor(0, -1)
-      }
-      active = nextActive.toList.distinct
+      active = nextActive
     }
   }
 }
